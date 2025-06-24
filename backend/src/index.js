527a82770d161f4ae5a363bca58dc8a33d9f2df8
Habeeb <habebenasr4@gmail.com>
2025-06-24 23:25:36 +0300

    Update static file serving in index.js to use path.resolve for improved directory handling; define __dirname for better path management.

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 // Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
@@ -25,9 +25,9 @@ app.use('/api/messages', messageRoutes);
 
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/dist"));
+    app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(process.cwd(), "frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "..", "..", "frontend", "dist", "index.html"));
     });
 }
 
